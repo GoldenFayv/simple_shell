36f644c1e29d804ca3942933d0ceaaab40f08432
Dawan Ejomah <dawanejomah@gmail.com>
2023-09-19 18:36:11 -0700

    shell_entry_file

---
+++ shell_entry_file.c
@@ -0,0 +1,31 @@
+#include "shell_header.h"
+#include <stdbool.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+        char *cmd = NULL;
+        size_t length = 0;
+
+        for (;;)
+        {
+                unveil_prompt();
+                read_input_cmd(&cmd, &length);
+                if (_strcmp(cmd, "exit") == 0)
+                {
+                        break;
+                }
+                else if (_strcmp(cmd, "env") == 0)
+                {
+                        show_env();
+                        continue;
+                }
+                implement_cmd(cmd);
+        }
+        free(cmd);
+        return (0);
+}
