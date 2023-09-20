78e5764c11dc691dd3e69d583da2ef3b69c098f7
Dawan Ejomah <dawanejomah@gmail.com>
2023-09-19 18:36:53 -0700

    shell_exec

---
+++ shell_exec.c
@@ -0,0 +1,25 @@
+#include "shell_header.h"
+
+void implement_cmd(const char *cmd)
+{
+
+        pid_t child_process_id = fork();
+
+        if (child_process_id == -1)
+        {
+                perror("fork");
+                exit(EXIT_FAILURE);
+        }
+        else if (child_process_id == 0)
+        {
+            char *args[] = {(char *)cmd, NULL};
+
+            if (execve(cmd, args, NULL) == -1)
+            {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+                wait(NULL);
+}
