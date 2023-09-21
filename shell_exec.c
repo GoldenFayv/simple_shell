706936983ae07a8d6b2e0c227431ab8b89bbadfd
Ekechukwu Favour <favour.e2002@icloud.com>
2023-09-21 00:28:16 +0000

    shell

--- shell_exec.c
+++ shell_exec.c
@@ -14,8 +14,9 @@ void implement_cmd(const char *cmd)
 	}
 	else if (child_process_id == 0)
 	{
-		char *args[] = {(char *)cmd, NULL};
-
+		char *args[2];
+		args[0] = (char *)cmd;
+		args[1] = NULL;
 		if (execve(cmd, args, NULL) == -1)
 		{
 			perror("execve");
